c4e612c66f6aa0e8e88986d7c0ecd7d1908cb095
Mustapha Afkir <afkir01@gmail.com>
2022-04-07 15:59:53 +0200

    install dependencies and remove boilerplate code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TuiRootModule } from "@taiga-ui/core";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,8 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    TuiRootModule,
+    BrowserAnimationsModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
